85e8a3f7c0ba7580b6286b8b9c0d74cf2534e1c6
A-spec-bt <70282195+A-spec-bt@users.noreply.github.com>
2021-12-15 15:08:28 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,56 @@
+// Menu-burger
+$(document).ready(function() {
+    $('.header__burger').click(function(event) {
+        $('.header__burger,.header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
+//Swiper
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
+//Scroll
+SmoothScroll({
+        // Время скролла 400 = 0.4 секунды
+        animationTime: 800,
+        // Размер шага в пикселях 
+        stepSize: 75,
+
+        // Дополнительные настройки:
+
+        // Ускорение 
+        accelerationDelta: 30,
+        // Максимальное ускорение
+        accelerationMax: 2,
+
+        // Поддержка клавиатуры
+        keyboardSupport: true,
+        // Шаг скролла стрелками на клавиатуре в пикселях
+        arrowScroll: 50,
+
+        // Pulse (less tweakable)
+        // ratio of "tail" to "acceleration"
+        pulseAlgorithm: true,
+        pulseScale: 4,
+        pulseNormalize: 1,
+
+        // Поддержка тачпада
+        touchpadSupport: true,
+    })
+    //Scroll id
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault()
+
+        const blockID = anchor.getAttribute('href').substr(1)
+
+        document.getElementById(blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    })
+}
